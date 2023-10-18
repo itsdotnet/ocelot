0c288832cad7f5ae880d31ca0f9355b04990725f
Iskandar Qodirov <eiscocyber@gmail.com>
2023-10-18 10:05:15 +0500

    Delete src/UserService.Api directory

--- src/UserService.Api/Controllers/UsersController.cs
+++
@@ -1,6 +0,0 @@
-namespace UserService.Api.Controllers;
-
-public class UsersController
-{
-    
-}
--- src/UserService.Api/Entities/User.cs
+++
@@ -1,9 +0,0 @@
-namespace UserService.Api.Entities;
-
-public class User
-{
-    public long Id { get; set; }
-    public string Firstnama { get; set; }
-    public string Lastname { get; set; }
-    public DateTime DateOfBirth { get; set; }
-}
